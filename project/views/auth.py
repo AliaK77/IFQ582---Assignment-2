1b4e65d2db2f7abcd0663eb5677ef1efe66e0d5e
Joshua Beck <dev@jbeck.com.au>
2026-06-27 21:32:30 +0800

    Login functionality

--- project/views/auth.py
+++ project/views/auth.py
@@ -1,8 +1,8 @@
 ### REF: IFQ582-5.8
-### import flask and blueprint / route template
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+### import blueprint / route template
+from flask import Blueprint, render_template, request, flash, redirect, session, url_for
 from hashlib import sha256
-from ..forms import RegisterForm
+from ..forms import RegisterForm, LoginForm
 from ..db.user import check_for_user, add_user
 
 bp = Blueprint('auth', __name__)
@@ -27,3 +27,31 @@ def register():
             return redirect(url_for('main.login'))
 
     return render_template('register.html', form=form)
+
+
+@bp.route('/login/', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            # Get hashed form of the submitted password
+            assert form.password.data
+            form.password.data = sha256(form.password.data.encode()).hexdigest()
+            # Compare with our database data
+            user = check_for_user(form.email.data, form.password.data)
+            if not user:
+                flash('Invalid username or password', 'error')
+                return redirect(url_for('main.login'))
+
+            # Store full user info in session
+            session['user'] = {
+                'firstname': user.first_name,
+                'surname': user.last_name,
+                'email': user.email,
+                'phone': user.phone,
+            }
+            session['logged_in'] = True
+            flash('Login successful!')
+            return redirect(url_for('main.index'))
+
+    return render_template('login.html', form=form)
