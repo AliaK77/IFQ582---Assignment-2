e1b26f0c25f690634d9770d253d50dfc8d8cd780
Joshua Beck <dev@jbeck.com.au>
2026-06-27 17:10:23 +0800

    Rename curator to staff

--- project/models/library_staff.py
+++ project/models/library_staff.py
@@ -1,16 +1,16 @@
-class LibraryCurator:
+class LibraryStaff:
     # Constructor
-    def __init__(self, curator_id, full_name, email):
-        self.curator_id = curator_id
+    def __init__(self, staff_id, full_name, email):
+        self.staff_id = staff_id
         self.full_name = full_name
         self.email = email
         self.collection_items = []   
 
 
-    #  Get curator details
-    def get_curator_details(self):
+    #  Get staff details
+    def get_staff_details(self):
         return {
-            "Curator ID": self.curator_id,
+            "staff ID": self.staff_id,
             "Full Name": self.full_name,
             "Email": self.email
         }
@@ -19,9 +19,9 @@ class LibraryCurator:
     #  __repr__ 
     def __repr__(self):
         return (
-            f"\nLibrary Curator\n"
+            f"\nLibrary staff\n"
             f"------------------------\n"
-            f"ID: {self.curator_id}\n"
+            f"ID: {self.staff_id}\n"
             f"Name: {self.full_name}\n"
             f"Email: {self.email}\n"
             f"Number of Items Managed: {len(self.collection_items)}\n"
